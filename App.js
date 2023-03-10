6cd3ea15306e8dad8d9b135289066dd87633cea0
Autumn Parker <agmparker@gmail.com>
2023-03-09 19:34:39 -0600

    FlatList Cont.
    - Added more to the FlatList including items it will render and output into text components
    - Added an array of placeholder tasks to see how it displays until i get the input functioning

--- App.js
+++ App.js
@@ -2,16 +2,31 @@ import React, {useState} from 'react';
 import {View, Text, StyleSheet, FlatList} from 'react-native';
 
 export default function App() {
+  // Array of tasks for testing purposes
+  const [todos, setTodos] = useState([
+    {text: 'task 1', key: '1'},
+    {text: 'task 2', key: '2'},
+    {text: 'task 3', key: '3'},
+  ]);
+
   return (
     // Views are like divs - they wrap elements in a container together
     <View style={styles.container}>
       {/* Header */}
       <View style={styles.content}>
         {/* Contains full list content */}
-        <View style={styles.list}>{/* Contains the list items */}
-        {/* FlatList needs specific props - check them in documentation Arlin linked */}
-        <FlatList data={todos}
-        renderItem={() => ()} />
+        <View style={styles.list}>
+          {/* Contains the list items */}
+          {/* FlatList needs specific props - check them in documentation Arlin linked */}
+          {/* Needs data prop and renderItem function */}
+          <FlatList
+            data={todos}
+            // renderItem function renders each item for the list
+            renderItem={({item}) => (
+                // Output the item with text component
+            <Text>{item.text}</Text>
+            )}
+          />
         </View>
       </View>
     </View>
