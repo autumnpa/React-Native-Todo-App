52ca7f4283453ec200227a920d4f4b29aaf90695
Autumn Parker <agmparker@gmail.com>
2023-03-10 10:46:01 -0600

    AddTodo Button Component
    - Updated the component to add new todo items
    - Changed styling

--- components/AddTodo.js
+++ components/AddTodo.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 
-const AddTodo = ({onPress}) => {
-  return (
-    <Button
-      title="+"
-      onPress={onPress}
-      color="#f1c40f"
-      accessibilityLabel="Add a new todo"
-      style={styles.button}
-    />
-  );
+const AddTodo = props => {
+  return <Button title="+" onPress={props.onPress} style={styles.button} />;
 };
 
 const styles = StyleSheet.create({
   button: {
-    borderRadius: 50,
-    width: 50,
-    height: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#007AFF',
+    fontSize: 20,
   },
 });
 
